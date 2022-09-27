3cdd022ac18dec6d85d812742f6c287184f9f569
Telephoner <yougotacall@gmail.com>
2022-09-27 17:51:26 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Пишем наше дз!");
+        System.out.println("Коммитим + пуш");
+        System.out.println("Переходим в браузер на гитхаб");
+    }
+}
